8f2681d97d1c7aa9bfc148d98544ef3690ae2ada
barkley-wq <igonyibarkley00@gmail.com>
2024-08-14 14:21:35 +0300

    index.js

---
+++ src/components/Filter.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Filter ({onCategoryChange}){
+
+    return (
+        <select name="filter" onChange={onCategoryChange} value = "All">
+            <option value="All">Filter by category</option>
+            <option value="Produce">Produce</option>
+            <option value="Dairy">Dairy</option>
+            <option value="Dessert">Dessert</option>
+        </select>
+    )
+}
